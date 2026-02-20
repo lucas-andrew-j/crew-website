1eaf9edf45a9fc5c99142ce42009264d6741fa12
Andrew <lucas.andrew.j@proton.me>
2026-02-19 18:07:14 -0800

    Add content to venturing page

--- src/routes/venturing/+page.svelte
+++ src/routes/venturing/+page.svelte
@@ -4,17 +4,35 @@
 
 <title>VENTURING</title>
 <div class="page" style="background-image:url({venturing_background});">
-	UNDER CONSTRUCTION
+	<div class="title">VENTURING: Crew 1548</div>
+	<div id="venturing-content"><p>Venturing is a youth-led leadership program that is designed for young adults that want
+		to test their limits and expand the bounds of what is possible. Crew 1548 was formed to connect friends and family
+		around high adventure.</p>
+		<p>The Venturing program is divided into four areas: Adventure, Leadership, Personal Growth, and Service. Each of
+			these
+			pillars represent a unique aspect of the Venturing program. In a Crew’s program, the focus is to provide
+			opportunities in all four areas.</p>
+		<p>Crew 1548 works very closely with Olympic Adventure Experience. Their main focus is mountaineering.</p>
+	</div>
 </div>
 
 <style>
     .page {
         flex: 1;
-        background-size:cover;
-        background-position:center;
-        display:flex;
-        flex-direction:column;
-        align-items:center;
-        justify-content:center;
+        background-size: cover;
+        background-position: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        /*justify-content:center;*/
+    }
+
+    .title {
+        color: white;
+        width: 50%;
+    }
+
+    #venturing-content {
+        width: 50%;
     }
 </style>
