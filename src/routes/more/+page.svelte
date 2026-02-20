c97dd71f9bfb614baf17d1e42717753859eb84da
Andrew <lucas.andrew.j@proton.me>
2026-02-19 16:34:52 -0800

    Adds components for remaining routes

---
+++ src/routes/more/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<title>MORE</title>
+<div class="page">
+	UNDER CONSTRUCTION
+</div>
+
+<style>
+    .page {
+        flex: 1;
+        background-size:cover;
+        background-position:center;
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+        justify-content:center;
+    }
+</style>
---
+++ src/routes/past-events/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<title>PAST EVENTS</title>
+<div class="page">
+	UNDER CONSTRUCTION
+</div>
+
+<style>
+    .page {
+        flex: 1;
+        background-size:cover;
+        background-position:center;
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+        justify-content:center;
+    }
+</style>
