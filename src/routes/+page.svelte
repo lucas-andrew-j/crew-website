90b5584fe88cf42563c3db0d098d6b0eb5e9ac11
Andrew <lucas.andrew.j@proton.me>
2026-02-19 17:29:26 -0800

    Formats content of home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,22 +4,61 @@
 
 <title>HOME</title>
 <div class="page" style="background-image:url({home_background});">
-	<div>
-		LEAD THE ADVENTURE
-	</div>
-	<div>
-		ABOUT
+	<div class="container">
+		<div id="message">
+			LEAD THE ADVENTURE
+		</div>
+		<a id="about-button" href="/about">
+			ABOUT
+		</a>
 	</div>
 </div>
 
 <style>
-		.page {
-				flex: 1;
-				background-size:cover;
-				background-position:center;
-				display:flex;
-				flex-direction:column;
-				align-items:center;
-				justify-content:center;
-		}
+    .page {
+        flex: 1;
+        background-size: cover;
+        background-position: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .container {
+        padding: 2em;
+        background-size: cover;
+        background-position: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: rgba(0, 0, 0, 0.5);
+				position: relative;
+				top: 5vh;
+    }
+
+    #message {
+        font-size: 2em;
+        font-weight: 900;
+        color: white;
+        letter-spacing: .08em;
+        margin-bottom: 1.5em;
+    }
+
+    #about-button {
+        text-align: center;
+        align-content: center;
+        color: white;
+        text-decoration: none;
+        border: 1px solid white;
+        min-width: 11em;
+        min-height: 4em;
+        margin-top: 1.5em;
+    }
+
+    #about-button:hover {
+        color: black;
+        background-color: white;
+    }
 </style>
