5954c88ed30f1e79e9ff5de29224aba596aaea86
Andrew <lucas.andrew.j@proton.me>
2026-02-26 23:03:40 -0800

    Adds first section to donate page

--- src/routes/donate-volunteer/+page.svelte
+++ src/routes/donate-volunteer/+page.svelte
@@ -1,20 +1,36 @@
 <script>
 	import default_background from '$lib/assets/default_background.jpg';
+	import donate_one from '$lib/assets/donate_one.jpg';
 </script>
 
 <title>DONATE/VOLUNTEER</title>
 <div class="page" style="background-image:url({default_background});">
-	UNDER CONSTRUCTION
+	<div class="content">
+		<h1 class="dark">Donate to Olympic Adventure Experience</h1>
+		<div style="display: flex; direction: row;"><a href="https://square.link/u/iPuHNjrT"><img src="{donate_one}"
+																																															style="width: 20rem;" />Donate with Square!</a>
+			<div><p class="dark">OAE needs financial support to do the work that we do. We need to purchase
+				gear. We need to pay for access permits, for food and transportation on our events, or
+				sometimes for guide services. There's also Scouting America membership fees, liability
+				insurance, and more.</p>
+				<p class="dark">We aim to lower the financial barrier of entry into mountaineering and other
+					high adventure activities. We will never turn away an otherwise prepared youth because they
+					can't pay for a trip, but we need your help to do that.</p>
+				<p class="dark">Every dollar helps, and every penny we receive will go directly into our
+					program.</p>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style>
     .page {
         flex: 1;
-        background-size:cover;
-        background-position:center;
-        display:flex;
-        flex-direction:column;
-        align-items:center;
-        justify-content:center;
+        background-size: cover;
+        background-position: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
     }
 </style>
