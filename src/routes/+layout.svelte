2547667c5275a5bec06de3cdd21b486616cc131c
Andrew <lucas.andrew.j@proton.me>
2026-08-24 21:47:12 -0700

    Adds initial pass on root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,38 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { page } from '$app/state';
+
+	const pages = [{ label: 'HOME', path: '' },
+		{ label: 'VENTURING', path: 'venturing' },
+		{ label: 'ABOUT', path: 'about' },
+		{ label: 'PAST EVENTS', path: 'past-events' },
+		{ label: 'FUTURE ADVENTURES', path: 'future-adventures' },
+		{ label: 'DONATE/VOLUNTEER', path: 'donate-volunteer' },
+		{ label: 'CONTACT', path: 'contact' },
+		{ label: 'REGISTRATION', path: 'registration' },];
+
+	let currentPage = $derived(page.url.pathname.split('/')[1]);
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<header>
+	<nav class="relative flex items-center py-4 px-6 border-b border-gray-200 bg-white">
+		<div class="flex items-center gap-2 shrink-0">OLYMPIC<br />
+			ADVENTURE<br />
+			EXPERIENCE
+		</div>
+		<div class="absolute left-1/2 -translate-x-1/2 hidden md:flex items-center">
+			{#each pages as page (page.label)}
+				<a href="/{page.path}" aria-current="{currentPage === page.path}" class="w-34 h-16 flex items-center text-center justify-center text-base text-blue-900 hover:text-white hover:bg-blue-900 transition-colors">{page.label}</a>
+			{/each}
+		</div>
+	</nav>
+</header>
+
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import home_background from '$lib/assets/home_background.jpg';
+</script>
+
+<title>HOME</title>
+<div class="page" style="background-image:url({home_background});">
+	<div class="container">
+		<div id="message">
+			LEAD THE ADVENTURE
+		</div>
+		<a id="about-button" href="/about">
+			ABOUT
+		</a>
+	</div>
+</div>
