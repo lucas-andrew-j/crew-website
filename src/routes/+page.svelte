d47dec97030aae78895326d2871f64c5eb361e92
Andrew <lucas.andrew.j@proton.me>
2026-08-25 17:32:29 -0700

    Styles content for main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,35 @@
 </script>
 
 <title>HOME</title>
-<div class="page" style="background-image:url({home_background});">
-	<div class="container">
-		<div id="message">
+<main class="flex flex-1 justify-center items-center">
+	<img src={home_background} alt="people camping on snow" fetchpriority="high"
+			 class="absolute inset-0 -z-10 h-full w-full object-cover" />
+	<div style="padding: 2em;
+        background-size: cover;
+        background-position: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: rgba(0, 0, 0, 0.5);
+				position: relative;
+				top: 5vh;">
+		<div style="font-size: 2em;
+        font-weight: 900;
+        color: white;
+        letter-spacing: .08em;
+        margin-bottom: 1.5em;">
 			LEAD THE ADVENTURE
 		</div>
-		<a id="about-button" href="/about">
+		<a style="text-align: center;
+        align-content: center;
+        color: white;
+        text-decoration: none;
+        border: 1px solid white;
+        min-width: 11em;
+        min-height: 4em;
+        margin-top: 1.5em;" href="/about">
 			ABOUT
 		</a>
 	</div>
-</div>
+</main>
