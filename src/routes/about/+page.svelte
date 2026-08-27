43d97b1db27c4be08f543ec4ec2c6e73eac796d9
Andrew <lucas.andrew.j@proton.me>
2026-08-26 21:29:33 -0700

    Adds about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import venturing_background from '$lib/assets/about_background.jpg';
+</script>
+
+<title>VENTURING</title>
+<main class="flex-col flex-1 justify-center">
+	<img src={venturing_background} alt="looking south from mt adams" fetchpriority="high"
+			 class="absolute inset-0 -z-10 h-full w-full object-cover" />
+	<div class="prose mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 mt-8 [&_p]:text-default-blue">
+		<h1 class="text-default-blue">About Olympic Adventure Experience</h1>
+		<p>Olympic Adventure Experience (OAE) is a non-profit organization that helps facilitate leadership opportunities
+			for local scouting organizations in the Kitsap County region. We seek to expand young people's comfort zones by
+			challenging them physically and mentally in outdoor environments.</p>
+		<p>OAE currently empowers Crew 1548 to reach higher levels of adventure.</p>
+	</div>
+</main>
