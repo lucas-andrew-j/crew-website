77b07395cbe4e7794412326e32fca49823a3d1cd
Andrew <lucas.andrew.j@proton.me>
2026-08-30 00:17:51 -0700

    Adds events table to database

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,17 @@
-import { pgTable, serial, integer, text } from 'drizzle-orm/pg-core';
+import { pgTable, integer, varchar, timestamp, check, index } from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
 
-export const task = pgTable('task', {
-	id: serial('id').primaryKey(),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1)
-});
+export const events = pgTable('events', {
+    id: integer('id').primaryKey().generatedByDefaultAsIdentity(),
+    name: varchar('name', {length: 50}).notNull(),
+    startDateTime: timestamp('start_date_time', {withTimezone: true}).notNull(),
+    endDateTime: timestamp('end_date_time', {withTimezone: true}).notNull(),
+	createdAt: timestamp('created_at', {withTimezone: true}).notNull().defaultNow(),
+	updatedAt: timestamp('updated_at', {withTimezone: true}).notNull().defaultNow(),
+}, (table) => [
+	check('valid_date_range', sql`${table.endDateTime} >= ${table.startDateTime}`),
+	index('start_date_time_idx').on(table.startDateTime),
+	index('end_date_time_idx').on(table.endDateTime),
+]);
 
 export * from './auth.schema';
