cb18b34db8970f4c732bd416ee79a5df617ebf7b
Andrew <lucas.andrew.j@proton.me>
2026-08-31 21:58:04 -0700

    Adds filter and order by for future adventures

--- src/routes/future-adventures/+page.server.ts
+++ src/routes/future-adventures/+page.server.ts
@@ -2,11 +2,14 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { eventsTable } from '../../lib/server/db/schema';
 import { error } from '@sveltejs/kit';
+import { sql } from 'drizzle-orm';
 
 const db = drizzle(process.env.DATABASE_URL!);
 
 export async function load() {
-	const events = await db.select().from(eventsTable);
+	const events = await db.select().from(eventsTable)
+		.where(sql`${eventsTable.endDateTime} >= CURRENT_TIMESTAMP`)
+		.orderBy(eventsTable.startDateTime);
 
 	if (!events) error(404);
 
