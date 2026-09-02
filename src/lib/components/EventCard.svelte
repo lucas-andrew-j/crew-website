c57f4b44c7cf0cc876ec50454b2fb3727491d5c7
Andrew <lucas.andrew.j@proton.me>
2026-09-01 22:26:21 -0700

    Groups events in future adventures by month

--- src/lib/components/EventCard.svelte
+++ src/lib/components/EventCard.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class="p-2">
-    <h2>{event.name}</h2>
+    <h3>{event.name}</h3>
     <p class="indent-2">{event.startDateTime.toLocaleDateString()}</p>
 </div>
--- src/routes/future-adventures/+page.svelte
+++ src/routes/future-adventures/+page.svelte
@@ -1,8 +1,24 @@
-<script>
+<script lang="ts">
     import default_background from '$lib/assets/default_background.jpg';
     import EventCard from "$lib/components/EventCard.svelte";
 
     let { data } = $props();
+
+    let eventMonths: number[][] = [];
+    let month = 0;
+
+    for (let i = 0; i < data.events.length; i++) {
+        if (i == 0) {
+            eventMonths[month] = [];
+            eventMonths[month].push(i);
+        } else if (new Date(data.events[i].startDateTime).getMonth() === new Date(data.events[i - 1].startDateTime).getMonth()) {
+            eventMonths[month].push(i);
+        } else {
+            month++;
+            eventMonths[month] = [];
+            eventMonths[month].push(i);
+        }
+    }
 </script>
 
 <title>FUTURE ADVENTURES</title>
@@ -10,8 +26,11 @@
     <img src={default_background} alt="looking north at Mt Hood, Mt Adams, and Mt St Helens" fetchpriority="high"
          class="absolute inset-0 -z-10 h-full w-full object-cover" />
     <div class="p-8 bg-cover bg-center flex flex-col">
-        {#each data.events as event (event.id)}
-            <EventCard {event}/>
+        {#each eventMonths as eventMonth}
+            <h2>{data.events[eventMonth[0]].startDateTime.toLocaleString('default', { month: 'long'})}</h2>
+            {#each eventMonth as event}
+                <EventCard event={data.events[event]}/>
+            {/each}
         {/each}
     </div>
 </main>
