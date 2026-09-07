1a6cdd8efa14645d656e196d28bfcb794a153c8c
Andrew <lucas.andrew.j@proton.me>
2026-09-06 18:59:47 -0700

    Adds email functionality to contact form

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,4 +1,9 @@
 import { TURNSTILE_SECRET_KEY } from '$env/static/private';
+import { RESEND_API_KEY } from '$env/static/private';
+import { Resend } from 'resend';
+import { escapeHtml } from '$lib/utils/sanitize.ts';
+
+const resend = new Resend(RESEND_API_KEY);
 
 async function validateTurnstile(token, remoteip) {
 	const formData = new FormData();
@@ -38,6 +43,30 @@ export const actions = {
 			console.log(data);
 		} else {
 			console.log('Turnstile validation error:', validation['error-codes']);
+			return;
+		}
+
+		if (!data.get('name') || !data.get('email') || !data.get('message')) {
+			console.log('One or more required fields is null');
+			return;
+		}
+
+		const { success, error } = await resend.emails.send({
+			from: 'Olympic Adventure Experience <onboarding@resend.dev>',
+			to: ['ajcoconut@proton.me'],
+			replyTo: data.email,
+			subject: `New contact form submission from ${data.get('name')}`,
+			html: `
+				<p><strong>Name:</strong> ${data.get('name')}</p>
+				<p><strong>Email:</strong> ${data.get('email')}</p>
+				<p><strong>Phone:</strong> ${data.get('phone')}</p>
+				<p><strong>Message:</strong></p>
+				<p>${data.get('message')}</p>
+			`
+		});
+
+		if (error) {
+			return console.error({ error });
 		}
 	}
 };
