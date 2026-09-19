cddc7e07dd848f4d04c81b0c4c0d44ee9996bbd4
Andrew <lucas.andrew.j@proton.me>
2026-09-18 19:13:09 -0700

    Fixes redundant drizzle clients in pages

--- src/routes/future-adventures/+page.server.ts
+++ src/routes/future-adventures/+page.server.ts
@@ -1,10 +1,7 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { db } from '$lib/server/db';
 import { eventsTable } from '../../lib/server/db/schema';
 import { error } from '@sveltejs/kit';
 import { sql } from 'drizzle-orm';
-
-const db = drizzle(process.env.DATABASE_URL!);
 
 export async function load() {
 	const events = await db.select().from(eventsTable)
@@ -12,7 +9,7 @@ export async function load() {
 		.orderBy(eventsTable.startDateTime);
 
 	if (!events) error(404);
-
+git 
 	return {
 		events
 	};
--- src/routes/past-events/+page.server.ts
+++ src/routes/past-events/+page.server.ts
@@ -1,13 +1,12 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { db } from '$lib/server/db';
 import { eventsTable } from '../../lib/server/db/schema';
 import { error } from '@sveltejs/kit';
 import { sql, desc } from 'drizzle-orm';
 
-const db = drizzle(process.env.DATABASE_URL!);
-
 export async function load() {
-	const events = await db.select().from(eventsTable)
+	const events = await db
+		.select()
+		.from(eventsTable)
 		.where(sql`${eventsTable.endDateTime} < CURRENT_TIMESTAMP`)
 		.orderBy(desc(eventsTable.startDateTime));
 
