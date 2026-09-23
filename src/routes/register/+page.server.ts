253d1ae9ab64d54d0cb85954543cbfc779eda720
Andrew <lucas.andrew.j@proton.me>
2026-09-22 22:53:30 -0700

    Adds registation page and functionality

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,85 @@
+import { TURNSTILE_SECRET_KEY } from '$env/static/private';
+import { RESEND_API_KEY } from '$env/static/private';
+import { Resend } from 'resend';
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { auth } from '$lib/server/auth';
+import { APIError } from 'better-auth/api';
+import { isAPIError } from '@better-auth/core/utils/is-api-error';
+
+const resend = new Resend(RESEND_API_KEY);
+
+async function validateTurnstile(token, remoteip) {
+	const formData = new FormData();
+	formData.append('secret', TURNSTILE_SECRET_KEY);
+	formData.append('response', token);
+	formData.append('remoteip', remoteip);
+
+	try {
+		const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+			method: 'POST',
+			body: formData
+		});
+
+		const result = await response.json();
+		return result;
+	} catch (error) {
+		console.error('Turnstile validation error:', error);
+		return { success: false, 'error-codes': ['internal-error'] };
+	}
+}
+
+export const load: PageServerLoad = (event) => {
+	if (event.locals.user) {
+		return redirect(302, '/');
+	}
+	return {};
+}
+
+export const actions: Actions = {
+	default: async ({ request }) => {
+		const data = await request.formData();
+
+		const token = data.get('cf-turnstile-response');
+		const ip =
+			request.headers.get('CF-Connecting-IP') ||
+			request.headers.get('X-Forwarded-For') ||
+			'unknown';
+
+		const validation = await validateTurnstile(token, ip);
+
+		if (!validation.success) {
+			console.log('Turnstile validation error:', validation['error-codes']);
+			return fail(400, { message: 'Turnstile validation failed' });
+		}
+
+		const firstName = data.get('firstName')?.toString() ?? '';
+		const lastName = data.get('lastName')?.toString() ?? '';
+		const email = data.get('email')?.toString() ?? '';
+		const password = data.get('password')?.toString() ?? '';
+
+		if (!firstName || !lastName || !email || !password) {
+			return fail(400, { message: 'One or more required fields is empty' });
+		}
+
+		try {
+			await auth.api.signUpEmail({
+				body: {
+					email,
+					password,
+					name: `${firstName} ${lastName}`,
+					firstName,
+					lastName,
+				}
+			});
+		} catch (error) {
+			if (isAPIError(error)) {
+				return fail(400, {message: error.message || 'Registration failed' });
+			}
+			console.error(error);
+			return fail(500, { message: 'Unexpected error' });
+		}
+
+		return redirect(302, '/');
+	}
+};
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import default_background from '$lib/assets/default_background.jpg';
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	let { form }: { form: ActionData } = $props();
+</script>
+
+<svelte:head>
+	<title>REGISTER</title>
+</svelte:head>
+
+<main class="flex flex-col flex-1">
+	<img src={default_background} alt="looking south from mt adams" fetchpriority="high"
+			 class="fixed inset-0 -z-10 h-full w-full object-cover"/>
+	<div class="prose mx-auto px-4 sm:px-6 mt-8">
+		<h1 class="text-default-blue">Register</h1>
+		<form method="POST" use:enhance class="flex flex-col [&_label]:flex [&_label]:flex-col [&_label]:pt-4">
+			<label>
+				First Name
+				<input
+					name="firstName"
+					required
+				/>
+			</label>
+			<label>
+				Last Name
+				<input
+					name="lastName"
+					required
+				/>
+			</label>
+			<label>
+				Email
+				<input
+					type="email"
+					name="email"
+					required
+				/>
+			</label>
+			<label>
+				Password
+				<input
+					type="password"
+					name="password"
+					required
+				/>
+			</label>
+			<div class="cf-turnstile pt-6"
+					 data-sitekey="0x4AAAAAAEp6CP5eujalxeMC"
+					 data-theme="light"
+					 data-size="normal"
+					 data-callback="onSuccess"></div>
+			{#if form?.message}
+				<p class="text-red-600">{form.message}</p>
+			{/if}
+			<button type="submit" class="bg-default-blue text-white mt-4 h-12 w-36">Register</button>
+			<a href="login">Need to login?</a>
+		</form>
+	</div>
+</main>
