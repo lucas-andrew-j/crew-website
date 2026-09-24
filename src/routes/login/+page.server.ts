4cdcebc90fb2b4716d8e29b2d86872bc34a5ce31
Andrew <lucas.andrew.j@proton.me>
2026-09-23 21:04:27 -0700

    Implements login page

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,74 @@
+import { TURNSTILE_SECRET_KEY } from '$env/static/private';
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { auth } from '$lib/server/auth';
+import { isAPIError } from '@better-auth/core/utils/is-api-error';
+
+async function validateTurnstile(token: string | null, remoteip: string) {
+	const formData = new FormData();
+	formData.append('secret', TURNSTILE_SECRET_KEY);
+	formData.append('response', token ?? '');
+	formData.append('remoteip', remoteip);
+
+	try {
+		const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+			method: 'POST',
+			body: formData
+		});
+
+		return await response.json();
+	} catch (error) {
+		console.error('Turnstile validation error:', error);
+		return { success: false, 'error-codes': ['internal-error'] };
+	}
+}
+
+export const load: PageServerLoad = (event) => {
+	if (event.locals.user) {
+		return redirect(302, '/');
+	}
+	return {};
+};
+
+export const actions: Actions = {
+	default: async ({ request }) => {
+		const data = await request.formData();
+
+		const token = data.get('cf-turnstile-response')?.toString() ?? null;
+		const ip =
+			request.headers.get('CF-Connecting-IP') ||
+			request.headers.get('X-Forwarded-For') ||
+			'unknown';
+
+		const validation = await validateTurnstile(token, ip);
+
+		if (!validation.success) {
+			console.log('Turnstile validation error:', validation['error-codes']);
+			return fail(400, { message: 'Turnstile validation failed' });
+		}
+
+		const email = data.get('email')?.toString() ?? '';
+		const password = data.get('password')?.toString() ?? '';
+
+		if (!email || !password) {
+			return fail(400, { message: 'One or more required fields is empty' });
+		}
+
+		try {
+			await auth.api.signInEmail({
+				body: {
+					email,
+					password,
+				}
+			});
+		} catch (error) {
+			if (isAPIError(error)) {
+				return fail(400, { message: error.message || 'Registration failed' });
+			}
+			console.error(error);
+			return fail(500, { message: 'Unexpected error' });
+		}
+
+		return redirect(302, '/');
+	}
+};
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,10 @@
-<script>
+<script lang="ts">
 	import default_background from '$lib/assets/default_background.jpg';
+	import type { ActionData } from '../../../.svelte-kit/types/src/routes/register/$types.d.ts';
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	let { form }: { form: ActionData } = $props();
 </script>
 
 <svelte:head>
@@ -9,12 +14,13 @@
 <main class="flex flex-col flex-1">
 	<img src={default_background} alt="looking south from mt adams" fetchpriority="high"
 			 class="fixed inset-0 -z-10 h-full w-full object-cover"/>
-	<div class="prose mx-auto px-4 sm:px-6 mt-8 [&_p]:text-default-blue [&_h3]:text-default-blue">
+	<div class="prose mx-auto px-4 sm:px-6 mt-8">
 		<h1 class="text-default-blue">Login</h1>
-		<form method="POST" class="flex flex-col [&_label]:flex [&_label]:flex-col [&_label]:pt-4">
+		<form method="POST" use:enhance class="flex flex-col [&_label]:flex [&_label]:flex-col [&_label]:pt-4">
 			<label>
 				Email
 				<input
+					type="email"
 					name="email"
 					required
 				/>
@@ -22,6 +28,7 @@
 			<label>
 				Password
 				<input
+					type="password"
 					name="password"
 				/>
 			</label>
@@ -30,6 +37,9 @@
 					 data-theme="light"
 					 data-size="normal"
 					 data-callback="onSuccess"></div>
+			{#if form?.message}
+				<p class="text-red-600">{form.message}</p>
+			{/if}
 			<button type="submit" class="bg-default-blue text-white mt-4 h-12 w-36">Login</button>
 			<a href="register">Need to register?</a>
 		</form>
